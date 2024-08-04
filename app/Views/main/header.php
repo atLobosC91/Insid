20efd4f90efe539e0d9794e638cabd46a8e571df
atLobosC91 <arlette.lobosc@outlook.com>
2024-08-04 16:15:23 -0400

    Assets

--- app/Views/main/header.php
+++ app/Views/main/header.php
@@ -17,15 +17,15 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="public\assets\vendor\animate.css\animate.min.css" rel="stylesheet">
+    <link href="public\assets\vendor\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+    <link href="public\assets\vendor\bootstrap-icons\bootstrap-icons.css" rel="stylesheet">
+    <link href="public\assets\vendor\boxicons\css\boxicons.min.css" rel="stylesheet">
+    <link href="public\assets\vendor\glightbox\css\glightbox.min.css" rel="stylesheet">
+    <link href="public\assets\vendor\swiper\swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="public\assets\css\style.css" rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: Groovin
@@ -44,7 +44,7 @@
 
             <!-- <h1 class="logo"><a href="index.html">Groovin</a></h1>-->
             <!-- Uncomment below if you prefer to use an image logo -->
-            <a href="index.html" class="logo"><img src="assets\img\logo.png" alt="" class="img-fluid"></a>
+            <a href="index.html" class="logo"><img src="/public/assets/img/logo.png" alt="" class="img-fluid"></a>
 
             <nav id="navbar" class="navbar">
                 <ul>
